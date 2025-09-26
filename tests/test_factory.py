0af0ab275d0208bcaaf904f077f9e781dfd5fd48
nicholas-taylor-quant <taylor.nicky@hotmail.com>
2025-09-26 17:51:16 +0200

    test(factory): update to use registered EuropeanOption and BlackScholes naming

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -2,6 +2,6 @@ from optionkit.core.factory import create_option, create_model
 
 def test_factory_creates_objects():
     opt = create_option("EuropeanOption", strike=100, maturity=1, is_call=True)
-    model = create_model("BlackScholesModel", spot=100, rate=0.05, vol=0.2)
+    model = create_model("BlackScholes", spot=100, rate=0.05, vol=0.2)
     assert opt.strike == 100
-    assert "BlackScholesModel" in repr(model)
+    assert "BlackScholes" in repr(model)
